a9b756565f54ba10cf2e5e9f29e3fa98202f355c
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-25 01:08:39 +0530

    pairs

---
+++ STL/pairs.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    //initialization 
+    pair<string,int>data;
+    //two method add data
+    data = make_pair("rohit", 30);
+    cout<<data.first<<"  "<<data.second<<endl;
+    return 0;
+}
