1a83e650d9b06850b09a34d716b275aee4a37b2e
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-25 01:31:01 +0530

    list methods

--- STL/list.cpp
+++ STL/list.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main()
 {
     // list
+    // methods
+    // push_front(), push_back() , size(), pop_front() , pop_back();
     list<int> ls;
     ls.push_back(20);
     ls.push_front(30);
     ls.push_back(40);
     ls.push_front(50);
+    ls.pop_back();
+    ls.pop_front();
     cout << ls.front() << " front value" << endl;
     cout << ls.back() << " back value" << endl;
     cout << ls.size() << "  size of doubly linked list" << endl;
