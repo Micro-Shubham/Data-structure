61ed84b982fc58b0ec5ec39df2802356d12210d9
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-26 14:08:06 +0530

    map

---
+++ STL/map.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // It store data  in key-value  pair
+    // It uses Red-black tree or AVL tree for implementation
+    //  insert,delete , search operation  O(log(n))
+    // duplicate element element will print two times
+    //  map create
+    map<int, int> m;
+    m.insert(make_pair(30, 40));
+    m.insert(make_pair(50, 40));
+    m.insert(make_pair(10, 40));
+    m.insert(make_pair(80, 40));
+    m.insert(make_pair(20, 40));
+    m.insert(make_pair(30, 40));
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        cout << it->first << "  " << it->second << endl;
+    }
+    return 0;
+}
