07fd33ae14e85473108e703e0c079a214eea663f
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-26 14:22:51 +0530

    multi map

---
+++ STL/multimap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // allow multi key
+    multimap<int, int> m;
+    m.insert(make_pair(20, 49));
+    m.insert(make_pair(20, 49));
+    m.insert(make_pair(10, 49));
+    m.insert(make_pair(10, 49));
+    m.insert(make_pair(40, 49));
+    m.insert(make_pair(50, 49));
+    m.insert(make_pair(30, 49));
+    for (auto it = m.begin(); it != m.end(); it++)
+    {
+        cout << it->first << "   " << it->second << endl;
+    }
+    return 0;
+}
