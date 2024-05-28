0104ea2f3a8687ac27ed02342fd2796cc5ec5695
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-28 16:25:31 +0530

    vector

--- vector/vector02.cpp
+++ vector/vector02.cpp
@@ -6,7 +6,12 @@ int main()
 {
     // v(size of array, each element is 1);
     vector<int> v(5, 1);
-    cout << "The size of v; " << v.size() << endl;;
-    cout << "The capacity of v: " << v.capacity();
+    cout << "The size of v; " << v.size() << endl;
+    cout << "The capacity of v: " << v.capacity() << endl;
+    vector<int> v1 = {1, 2, 3, 4, 5, 6, 7};
+    cout << "The size of v; " << v1.size() << endl;
+    cout << "The capacity of v: " << v1.capacity();
+    
+
     return 0;
 }
