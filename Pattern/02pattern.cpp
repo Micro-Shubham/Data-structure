f49eb015c72d542eb37c0c5fea7ae0280597b3dd
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-10-26 11:42:13 +0530

    02pattern

---
+++ Pattern/02pattern.cpp
@@ -0,0 +1,21 @@
+/*
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= 5; col++)
+        {
+            cout << 10 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
