df99a2220e408db64a49906f7aec1d26d132b1a9
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-03-03 13:10:22 +0530

    value of var using pointer

--- Pointers/01pointer.cpp
+++ Pointers/01pointer.cpp
@@ -7,7 +7,10 @@ int main()
     cout << &a << endl;
     // store the value address we need pointer
     int *ptr = &a;
+    // print the address of a
     cout << ptr << endl;
+    // print the value of a using pointer
+    cout << *ptr << endl;
 
     return 0;
 }
