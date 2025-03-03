10396f9390ca195f37bbafca8ebb4cf976468e39
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-03-03 16:34:42 +0530

    airthematic on pointer

---
+++ Pointers/Pointer in array/02arr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *ptr = arr;
+    // print all the value using pointer
+    // we can perform increament/decrement or airthmetic operation as well
+    for (int i = 0; i < 5; i++)
+    {
+        cout << ptr[i] << endl;
+    }
+    // print all the address
+    for(int i = 0; i < 5; i++)
+    {
+        cout << ptr+i << endl;
+    }
+    return 0;
+}
