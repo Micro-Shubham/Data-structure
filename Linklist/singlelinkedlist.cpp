b8a47ab707f3e0fc87b654facb3ce9c17cbbfab7
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-04-01 14:38:19 +0530

    add constructor to Node class for initializing data

--- Linklist/singlelinkedlist.cpp
+++ Linklist/singlelinkedlist.cpp
@@ -5,10 +5,16 @@ class Node
     public:
     int data;
     Node *next;
+    // constructor 
+    Node(int data )
+    {
+        this -> data = data;
+        this -> next = NULL;
+    }
 };
 int main()
 {
-    Node *node1 = new Node();
+    Node *node1 = new Node(10);
     cout << node1 ->data << endl;
     cout << node1 ->next << endl;
     return 0;
