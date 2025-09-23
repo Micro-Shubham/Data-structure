d8b2e52a23d33ab4f6eac7cbce1687f578f7b31f
Shubham Yadav <shubhamtikkha09@gmail.com>
2025-09-23 12:45:26 +0530

    LInkedlist basic knowledge

--- 14Linklist/01SinglyLL/01LL.cpp
+++ 14Linklist/01SinglyLL/01LL.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 struct Node
 {
-    int data;
-    Node *next;
-
+    int data; // data value
+    Node *next; // pointer to the next value
+    //
     Node(int data1, Node *next1)
     {
         data = data1;
@@ -17,9 +17,10 @@ int main()
 {
     vector<int> arr = {1, 4, 4, 5, 4};
     // creating a new node and what new does is it store the memory location 
-    Node* y = new Node(arr[0], nullptr);
-    // creating a object 
+    Node* y = new Node(arr[1], nullptr);
+    // creating a object
     Node x = Node(arr[1], nullptr);
     cout << y << endl;;
     cout << x.next<< endl;
+    cout << y->data<<endl;
 }
