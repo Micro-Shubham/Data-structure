cc3d16825067c0ee822c43b51bdd48e6e0ae0a0d
Shubham Yadav <shubhamtikkha09@gmail.com>
2026-04-19 11:01:31 +0530

    Add 01recursion.cpp file with basic recursive function structure

--- 15recursion/01recursion.cpp
+++ 15recursion/01recursion.cpp
@@ -1,7 +1,16 @@
 // A function that call itself again and again until certain condition meet
 #include <iostream>
 using namespace std;
+void fun(int n ) {
+    if(n == 0) {
+        cout<< "Happy Birthday!"<< endl;
+        return;
+    }
+    cout<<n << " days left for birthday"<< endl;
+    fun(n-1);
+}
 int main() {
- 
+  int n = 5;
+  fun(n);
     return 0;
 }
